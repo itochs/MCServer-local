c575abd9b42cc73f25944e21e5bdb3594d550417
itochs <halglobe0108@gmail.com>
2022-10-24 21:59:54 +0900

    change server class and add status

--- MCOperation.py
+++ MCOperation.py
@@ -1,25 +1,37 @@
-from server import Server
+# from server import Server
 from discord.ext import commands
+from serverstatus import ServerStatus
 
 class ServerOperation(commands.Cog):
     def __init__(self, bot) -> None:
         super().__init__()
         self.bot = bot
-        self.server = Server()
-
+        
 
     @commands.command()
     async def start(self, context):
+        if self.bot.server_status not in self.bot.allowed:
+            return
+
         await context.send("start")
-        for log in self.server.start("minecraft_server/server-v1.19/"):
+        self.bot.server_status = ServerStatus.starting
+        for log in self.bot.server.start("../minecraft_server/server-v1.19/"):
             print(log)
+        
+        self.bot.server_status = ServerStatus.waiting
 
 
     @commands.command()
     async def stop(self, context):
+        if self.bot.server_status not in self.bot.allowed:
+            return
+
         await context.send("stop")
-        for log in self.server.stop():
+        self.bot.server_status = ServerStatus.stopping
+        for log in self.bot.server.stop():
             print(log)
+        
+        self.bot.server_status = ServerStatus.stop
 
 
 def setup(bot):
